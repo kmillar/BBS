5824efd22fda59d8619acdb99286991c7c4a63ca
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-03-09 17:00:35 +0000

    fix syntax error
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioC/admin/build/BBS@100399 bc3139a8-67e5-0310-9ffc-ced21a209358

--- manage-BioC-repos/3.1/updateReposPkgs-data-experiment.sh
+++ manage-BioC-repos/3.1/updateReposPkgs-data-experiment.sh
@@ -6,7 +6,7 @@ cd "$HOME/manage-BioC-repos/3.1"
 
 BBS_OUTGOING_DIR="/home/biocbuild/public_html/BBS/$BIOC_VERSION/data-experiment/OUTGOING"
 R_SCRIPT="source('/home/biocbuild/BBS/utils/list.old.pkgs.R')"
-PROPAGATION_R_SCRIPT="/home/biocbuild/BBS/utils/createPropagationDB.R"
+PROPAGATION_R_SCRIPT="source('/home/biocbuild/BBS/utils/createPropagationDB.R')"
 PROPAGATION_DB_FILE="$BBS_OUTGOING_DIR/../PROPAGATE_STATUS_DB.txt"
 
 REPOS_ROOT="$HOME/PACKAGES/$BIOC_VERSION/data/experiment"
